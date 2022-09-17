56374504f1ccba098e451e0f36deb7b33afc1cd2
habib-ullah <habib4811@gmail.com>
2022-09-17 20:14:02 +0600

    added 4 routes

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion,ObjectId} = require('mongodb');
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+var nodemailer = require('nodemailer');
+var sgTransport = require('nodemailer-sendgrid-transport');
 
 app.use(cors());
 app.use(express.json());
+
 
 //jwt-token-to-backend-for-Verification
 const verifyJwt = async (req, res, next) => {
@@ -38,6 +42,7 @@ async function run(){
  const reviewCollection = client.db("fish-zone").collection("reviews");
  const userCollection = client.db("fish-zone").collection("users");
  const profileCollection = client.db("fish-zone").collection("profiles");
+ const paymentCollection = client.db("fish-zone").collection("payment");
 
 
  const verifyAdmin = async(req, res, next)=>{
@@ -51,7 +56,11 @@ async function run(){
     res.status(403).send({message: "forbidden"});
   }
 }
-
+app.post('/equitment', verifyJwt, verifyAdmin, async(req, res)=>{
+  const product = req.body;
+  const result = await partsCollection.insertOne(product);
+  res.send(result);
+})
 
 //GET DATA myOWN Inserted DATA
 app.get('/equitment', async (req, res) => {
@@ -61,7 +70,11 @@ app.get('/equitment', async (req, res) => {
   res.send(equitments);
 });
 
-
+   // get all products
+   app.get("/equitment", async (req, res) => {
+    const products = (await partsCollection.find({}).toArray()).reverse();
+    res.send(products);
+  });
 
 
 app.get("/equitment/:id", verifyJwt, async (req, res) => {
@@ -71,7 +84,19 @@ app.get("/equitment/:id", verifyJwt, async (req, res) => {
   res.send(product);
 });
 
+    // delete product api
+    app.delete("/equitment/:id", verifyJwt, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await partsCollection.deleteOne(filter);
+      res.send(result);
+    });
 
+   // get all orders for admin 
+   app.get('/orders', verifyJwt, verifyAdmin, async(req, res)=>{
+    const orders = await orderCollection.find({}).toArray();
+    res.send(orders);
+  })
 
 // add order api
 app.post("/order", verifyJwt, async (req, res) => {
@@ -96,6 +121,44 @@ app.post("/order", verifyJwt, async (req, res) => {
     res.send(result);
   });
 
+
+  app.patch("/order/:id", async (req, res) => {
+    const id = req.params.id;
+    const order = req.body;
+    const { orderId, transactionId } = order;
+    const filter = { _id: ObjectId(id) };
+    const updatedDoc = {
+      $set: {
+        paid: true,
+        transactionId: transactionId,
+      },
+    };
+    const result = await paymentCollection.insertOne({
+      orderId,
+      transactionId,
+    });
+    const updatedOrder = await orderCollection.updateOne(filter, updatedDoc);
+    res.send(updatedOrder);
+  });
+// order shipped api
+app.patch('/order/shipped/:id', verifyJwt, verifyAdmin, async(req, res)=>{
+  const id = req.params.id;
+  const filter = {_id: ObjectId(id)};
+  const updatedDoc = {
+    $set: {
+      shipped: true
+    }
+  }
+  const result = await orderCollection.updateOne(filter, updatedDoc);
+  res.send(result)
+})
+
+
+
+  app.get("/reviews", async (req, res) => {
+    const review = (await reviewCollection.find({}).toArray()).reverse();
+    res.send(review);
+  });
 // add review api
 app.post("/review", async (req, res) => {
   const review = req.body;
@@ -114,6 +177,24 @@ app.post('/profile', async (req, res) => {
   const profileInfo = req.body;
   const result = await profileCollection.insertOne(profileInfo);
   res.send(result);
+});
+
+app.get("/payment/:id", verifyJwt, async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: ObjectId(id) };
+  const order = await orderCollection.findOne(query);
+  res.send(order);
+});
+
+app.post("/create-payment-intent", async (req, res) => {
+  const { totalPrice } = req.body;
+  const amount = totalPrice * 100;
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount,
+    currency: "usd",
+    payment_method_types: ["card"],
+  });
+  res.send({ clientSecret: paymentIntent.client_secret });
 });
 
  // get all users api
@@ -212,5 +293,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Doctor app listening on port ${port}`)
+  console.log(`Manufacturer app listening on port ${port}`)
 })
